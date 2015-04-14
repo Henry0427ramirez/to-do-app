7bfefd0d0aefdbf3cfd8057aa8818cf03ccd65c4
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-14 08:54:20 -0700

    commit 2
    added more to index.php

--- index.php
+++ index.php
@@ -1,1 +1,13 @@
-index.php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Henry's To-Do List</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div class="wrap">
+		
+	</div>
+
+</body>
+</html>
