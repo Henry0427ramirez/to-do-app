9c6df53d78756f9d5cc8ebb41cc2ef041f090c57
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-15 19:50:34 -0700

    commit 4
    finished video 5

--- includes/connect.php
+++ includes/connect.php
@@ -2,7 +2,7 @@
 // why is this important? why is localhost so important?
 $mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
 //mysquil->connect_error then we want it to die and have this message
-if (mysqli-> connect_error) {
+if ($mysqli->connect_error) {
 	die('connect Error (' . $mysqli->connect_errno . ')' . $mysqli->connect_error);
 }
 else {
