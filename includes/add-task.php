533b91c8b70a474be6111a0fc5f3ec226d694bae
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-16 09:15:21 -0700

    commit 10
    finished video 11

---
+++ includes/add-task.php
@@ -0,0 +1,13 @@
+<?php 
+  $task = strip_tags($POST['tasks']);
+  $date = date('Y-m-d');
+  $time = date('H:i:s');
+
+  include('connect.php');
+
+  $mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+  $mysqli ->query("INSERT INTO tasks VALUES ('','$task', 'date', 'time')");
+
+  $query = "SELECT * FROM tasks WHERE tasks='$tasks' and date='$date' and time='$time'";  
+
+?>
