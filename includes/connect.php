1eebbe918f8fbf53bfb1c5009c15a5e2c92ccf33
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-15 19:57:23 -0700

    commit 4.1
    fixed the error from vid 5

--- includes/connect.php
+++ includes/connect.php
@@ -1,6 +1,6 @@
 <?php
 // why is this important? why is localhost so important?
-$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+$mysqli = new mysqli('localhost', 'root', 'root', 'todo');
 //mysquil->connect_error then we want it to die and have this message
 if ($mysqli->connect_error) {
 	die('connect Error (' . $mysqli->connect_errno . ')' . $mysqli->connect_error);
