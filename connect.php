c8b581cb693b421ce6ff13c8ef3f0af06f0d7bad
Henry Ramirez <h.ramirez0427@gmail.com>
2015-04-15 19:30:29 -0700

    commit 3
    finished video 4

---
+++ connect.php
@@ -0,0 +1,13 @@
+<?php
+// why is this important? why is localhost so important?
+$mysqli = new mysqli('localhost', 'root', 'root', 'tasks');
+//mysquil->connect_error then we want it to die and have this message
+if (mysqli-> connect_error) {
+	die('connect Error (' . $mysqli->connect_errno . ')' . $mysqli->connect_error);
+}
+else {
+	echo "Connection made";
+}
+$mysqli->close();
+
+?>
